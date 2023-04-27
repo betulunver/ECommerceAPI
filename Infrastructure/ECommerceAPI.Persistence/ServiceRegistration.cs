38577e210c57f37aaff86b4fede7298c29024dc4
betulunver <betulunver@outlook.com>
2023-04-27 23:46:38 +0300

    base folders and class libraries created.

---
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services) //Extension metod. Bunu da program.cs içine ekliyoruz.
+        {
+            services.AddSingleton<IProductService, ProductService>(); //ProdcutService dön.
+
+        }
+    }
+}
